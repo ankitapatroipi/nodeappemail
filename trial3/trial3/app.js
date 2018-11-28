4ef08c161af9bb07529d54a8d3a6b7d9c9746420
ankitapatroipi <ankitapatroipi@gmail.com>
2018-11-28 17:31:40 -0500

    mm

---
+++ trial3/trial3/app.js
@@ -0,0 +1,15 @@
+var events = require('events');
+
+var eventEmitter = new events.EventEmitter();
+
+var f1 = function () {
+    console.log("Test Event is happens.");
+};
+
+eventEmitter.on("TestEvent", f1);
+
+eventEmitter.emit("TestEvent");
+
+
+
+// JavaScript source code
